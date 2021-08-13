db71339ad9d8a1c41b819b892149bb24222fbbd4
liqiongfan <you@example.com>
2021-08-13 22:16:33 +0800

    Use stack instead of memory stack

--- zua_type.c
+++ zua_type.c
@@ -177,12 +177,11 @@ ZUA_API zval *zua_get_value(zval *v, const char *key, uint32_t key_len) {
 
 ZUA_API zval *zua_get_value_by_index(zval *v, uint32_t index) {
     zval *ret = NULL;
+    char keyBuf[MAX_STRING_KEY];
     if (Z_TYPE_P(v) == IS_ARRAY) {
-        char *keyBuf = zua_malloc(sizeof(char) * MAX_STRING_KEY);
         bzero(keyBuf, sizeof(char) * MAX_STRING_KEY);
         sprintf(keyBuf, "%d", index);
         ret = hashmap_get(Z_ARR_P(v), keyBuf, strlen(keyBuf));
-        zua_free(keyBuf);
     }
     return ret;
 }

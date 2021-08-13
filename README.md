e1973ca57f1b95feac63ebafa716a20857ea5851
李景港 <lijinggang@sdbattery.com>
2021-08-13 13:48:12 +0800

    Update README

--- README.md
+++ README.md
@@ -104,15 +104,23 @@ Zua 采用纯C语言开发, 可以在任意支持标准C的机器上运行，占
 + 解码
 
 ```C
-zval *response = json_decode("{ code: 200, message: "SUCCESS" }");
+zval *response = json_decode(ZUA_STR("{ code: 200, message: 'SUCCESS', language: ['Golang', 'C/C++'] }"));
 if (response->u2.errcode != 0) {
     printf("解析错误~");
     zval_free(response);
     return 0;
 }
 zval *code = zua_get_value(response, ZUA_STR("code"));
-printf("code:%ld\n", Z_LVAL_P(code));
+code != NULL && printf("code:%ld\n", Z_LVAL_P(code));
 
+zval *message = zua_get_value(response, ZUA_STR("message"));
+message != NULL && printf("message: %s\n", ZSTR_VAL(Z_STR_P(message)));
+
+zval *lang = zua_get_value(response, ZUA_STR("language"));
+if (lang != NULL) {
+    zval *favor_language = zua_get_value_by_index(lang, 0);
+    printf("Favor Language: %s\n", ZSTR_VAL(Z_STR_P(favor_language)));
+}
 zval_free(response);
 ```
 
--- main.c
+++ main.c
@@ -3,38 +3,25 @@
 
 int main(int argc, char *argv[]) {
 
-    char str[] = "{\n"
-                 "  // comments\n"
-                 "  unquoted: 'and you can quote me on that',\n"
-                 "  singleQuotes: 'I can use \"double quotes\" here',\n"
-                 "  lineBreaks: \"Look, Mom! \\\n"
-                 "No \\\\n's!\",\n"
-                 "  hexadecimal: 0xdecaf,\n"
-                 "  leadingDecimalPoint: .8675309, andTrailing: 8675309.,\n"
-                 "  positiveSign: +1,\n"
-                 "  trailingComma: 'in objects', andIn: ['arrays',],\n"
-                 "  \"backwardsCompatible\": \"with JSON\",\n"
-                 "}";
-
-    zval *r = json_decode(ZUA_STR(str));
-
-    if (r->u2.errcode == ZUA_JSON_SYNTAX_ERROR) {
-        printf("语法错误");
+    zval *response = json_decode(ZUA_STR("{ code: 200, message: 'SUCCESS', language: ['Golang', 'C/C++'] }"));
+    if (response->u2.errcode != 0) {
+        printf("解析错误~");
+        zval_free(response);
         return 0;
     }
-    if (r->u2.errcode == ZUA_JSON_BRACKET_MISMATCH) {
-        printf("JSON 括号不匹配~");
-        return 0;
+    zval *code = zua_get_value(response, ZUA_STR("code"));
+    code != NULL && printf("code:%ld\n", Z_LVAL_P(code));
+
+    zval *message = zua_get_value(response, ZUA_STR("message"));
+    message != NULL && printf("message: %s\n", ZSTR_VAL(Z_STR_P(message)));
+
+    zval *lang = zua_get_value(response, ZUA_STR("language"));
+    if (lang != NULL) {
+        zval *favor_language = zua_get_value_by_index(lang, 0);
+        printf("Favor Language: %s\n", ZSTR_VAL(Z_STR_P(favor_language)));
     }
 
-    zua_string *s = json_encode_pretty(r);
-
-    if (s != NULL) {
-        printf("%s\n", ZSTR_VAL(s));
-    }
-
-    zval_free(r);
-    zua_string_free(s);
+    zval_free(response);
 
     return 0;
 }
